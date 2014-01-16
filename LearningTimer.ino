52be0ceeb9ebf204251f6710c9570ab2d98431b0
Frank <frank@efsol.com>
2014-01-16 15:30:51 +0000

    used elapsed interval timing instead of delay() plus code
    to ensure that all paths through the main loop take the same cycle time
    
    also tidied docs

--- LearningTimer.ino
+++ LearningTimer.ino
@@ -2,34 +2,38 @@
   Learning Timer
   
  No fiddling with switches and displays to set how long to wait, just teach the timer 
- by clicking the LEARN button, waiting, and clicking it again. Once you have taugyht the
+ by clicking the LEARN button, waiting, and clicking it again. Once you have taught the
  timer, you can run it for that duration any time you like by pressing the RUN button. 
- At the end of the learned time, the buzzer wil sound. Simple
+ At the end of the learned time, the buzzer wil sound. Simple.
  
- If you connect 8 LEDS to pins 0-7 they will indicate the state of the system. While learning,
- a dot will "walk" along the array. While running, a "solid" bar will steadily decrease until
- the buzzer sounds. It's worth it, it looks neat.
+ As a bonus, if you connect 8 LEDS to pins 0-7 they will indicate the state of the system.
+ While learning, a dot will "walk" along the array. While running, a "solid" bar will
+ steadily decrease until the buzzer sounds. It's worth it, it looks neat.
 
  The circuit:
  * LEARN button (active HIGH) attached to pin 8
- * LEARN button (active HIGH) attached to pin 9
+ * RUN button (active HIGH) attached to pin 9
  * Piezo buzzer attached to pin 10 through a resistor
- * optional 8-LED array attached to pins 0-7
+ * (optional) 8-LED array attached to pins 0-7
  
+ Frank Carver, January 2014
  */
 
 const int learnButton = 8;
 const int runButton = 9;
 const int buzzPin =  10;
+
+// to avoid unintended very-short durations, times start from when you let go of the button
 enum { WAITING, LEARN_START_PRESS, LEARNING, LEARN_END_PRESS, RUN_PRESS, RUNNING } state;
 
-const unsigned int tick = 200; // ms
+const unsigned int tick = 100; // ms
 const unsigned int ticksPerSecond = 1000 / tick;
 const unsigned int buzzFreq = 1000;
 const unsigned long buzzDuration = 500;
 
 long goal;
 long count;
+long next_tick;
 
 void clear_dots() {
   for (int i = 0; i < 8; ++i) {
@@ -47,7 +51,7 @@ void hop() {
 void bar() {
   float progress = count / (goal * 1.0);
   int dots = (int)(8 * progress);
-  for (int i = 0; i < 8; ++i) digitalWrite(i, i < dots ? HIGH : LOW);
+  for (int i = 7; i >= 0; --i) digitalWrite(i, i < dots ? HIGH : LOW);
 }
 
 void setup() {
@@ -66,8 +70,7 @@ void setup() {
 }
 
 void loop(){
-  delay(tick);
-  ++count;
+  next_tick = millis() + tick;
   
   switch (state) {
     case WAITING:
@@ -80,8 +83,10 @@ void loop(){
     case LEARN_START_PRESS:
       if (digitalRead(learnButton) == LOW) {
         state = LEARNING;
+        dot = 0;
         count = 0;
       }
+      break;
     case LEARNING:
       hop();
       if (digitalRead(learnButton) == HIGH) {
@@ -111,4 +116,8 @@ void loop(){
       }
       break;
   }
+
+  while (millis() < next_tick) 
+    ;
+  ++count;
 }
